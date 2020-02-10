1a2a44fb425e4148a06b060ae016627ddd7a1299
Michelle Cupp <shellcupp@gmail.com>
2020-02-10 17:03:46 -0600

    mvp

--- src/App.js
+++ src/App.js
@@ -1,16 +1,89 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm'
+import TodoList from './components/TodoComponents/TodoList'
+import styled from 'styled-components';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Dishes',
+          id: 1,
+          completed: false
+        },
+        {
+          task: 'Feed Cats',
+          id: 2,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
+  addTodo = e => {
+    e.preventDefault();
+    const newTodo = { task: this.state.todo, completed: false, id: Date.now() };
+    this.setState({ 
+      todos: [...this.state.todos, newTodo], 
+      todo: '' 
+    });
+  };
+
+  changeTodo = e => this.setState({ [e.target.name]: e.target.value });
+
+  toggleTodoComplete = id => {
+    let todos = this.state.todos.slice();
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        return todo;
+      } else {
+        return todo;
+      }
+    });
+    this.setState({ todos });
+  };
+
+  clearCompletedTodos = e => {
+    e.preventDefault();
+    let todos = this.state.todos.filter(todo => !todo.completed);
+    this.setState({ todos });
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+      <Fancyh1>To Do List</Fancyh1>
+      <Fancyp>Add More Tasks below and cross them off when you are finished.</Fancyp>
+        <TodoList
+          handleToggleComplete={this.toggleTodoComplete}
+          todos={this.state.todos}
+        />
+        <TodoForm
+          value={this.state.todo}
+          handleTodoChange={this.changeTodo}
+          handleAddTodo={this.addTodo}
+          handleClearTodos={this.clearCompletedTodos}
+        />
       </div>
     );
   }
 }
+
+
+const Fancyh1 = styled.h1`
+color: pink;
+font-family: 'Sriracha', cursive;
+
+`;
+const Fancyp = styled.p`
+color:pink;
+font-family: 'Sriracha', cursive;
+`;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import CSS from './components/TodoComponents/Todo.css'
 
 render(<App />, document.getElementById('root'));
